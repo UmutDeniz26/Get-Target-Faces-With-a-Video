605a2e31457628aa57b69da0807c90506310df3b
Umut Deniz <umutcandeniz2001@gmail.com>
2023-01-25 12:30:56 +0300

    Master

--- takeSSdueToFace.py
+++ takeSSdueToFace.py
@@ -4,13 +4,16 @@ import pyautogui as pg
 import numpy as np
 import keyboard
 import asyncio
+import time
 import math
 import cv2
+import os
 
 def logLastImgNumber(number):
     f=open('log.txt','w')
     f.write(str(number))
     f.close()
+
 def getLastImgNumber():
     f=open('log.txt','r')
     data=f.readline()
@@ -18,40 +21,68 @@ def getLastImgNumber():
     return data
 
 
+#face_recognition.face_encodings(cv2.cvtColor(np.array(screenshotTemp),cv2.COLOR_RGB2BGR))
+
+def encodeTesters():
+    testerArr=[]
+    for names in os.listdir('tester'):
+        tester = cv2.cvtColor(cv2.imread("tester/{}".format(names)), cv2.COLOR_BGR2RGB)
+        #cv2.imwrite("Tester{}".format(names),tester)
+        tester_encoding = face_recognition.face_encodings(tester)[0]
+        testerArr.append(tester_encoding)
+    return testerArr
+
+def simpleTest(encodedScreenshot,testerArr):    
+    accuracyCnt=0
+    errorCnt=0
+    print("Test Begins :)")
+    for index, tester in enumerate(testerArr):
+        try:
+            if face_recognition.compare_faces([encodedScreenshot], tester):
+                print("Test {} passed !".format(index))
+                accuracyCnt+=1
+        except:
+            errorCnt+=1
+
+    print("AccuracyCnt: {}".format(accuracyCnt),"    ErrorCnt: {}".format(errorCnt))
+    return len(testerArr)-accuracyCnt # if all test accurate, returns 0
+
+
+encodedTesters = encodeTesters()
+
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
-tester = cv2.cvtColor(cv2.imread("tester/tester.jpg"), cv2.COLOR_BGR2RGB)
-tester_encoding = face_recognition.face_encodings(tester)[0]
 
+start = time.time()
 print("Started to capture ..!")
-async def execute_loop():
-    while True:
-        await asyncio.sleep(0.1)
+
+while True:  # making a loop
+    try:  # used try so that if user pressed other than the given key error will not be shown
         
-        #finds faces and its positions, also compares with images folder
-        face_locations, face_names =sfr.detect_known_faces(cv2.cvtColor(np.array(pg.screenshot()),cv2.COLOR_RGB2BGR)) # screenshot in known_faces()
-
-        for face_loc, face_names in zip(face_locations, face_names):
-            if face_names!="Unknown":
-                y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-                
-                offset=math.floor((x2-x1)/2)
-                screenshotTemp = pg.screenshot(region=(x1-offset,y1-offset,x2-x1+2*offset,y2-y1+2*offset))
-                
-                fileNum=getLastImgNumber()
-                print("  [x2-x1]: ",x2-x1,"  [y2-y1]: ",y2-y1,"   Name:",face_names," FileNum:",fileNum," base:{",x1,y1,x2,y2,"}")
-
-                try:
-                    if face_recognition.compare_faces([face_recognition.face_encodings(cv2.cvtColor(np.array(screenshotTemp),cv2.COLOR_RGB2BGR))], tester_encoding):
+        if time.time()-start > 0.2:
+            start=time.time()
+            face_locations, face_names =sfr.detect_known_faces(cv2.cvtColor(np.array(pg.screenshot()),cv2.COLOR_RGB2BGR)) # screenshot in known_faces()
+            for face_loc, face_names in zip(face_locations, face_names):
+                if face_names!="Unknown":
+                    y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+                    offset=math.floor((x2-x1)/2)
+                    screenshotTemp = pg.screenshot(region=(x1-offset,y1-offset,x2-x1+2*offset,y2-y1+2*offset))
+                    
+                    fileNum=getLastImgNumber()
+                    encodedTemp = face_recognition.face_encodings(cv2.cvtColor(np.array(screenshotTemp),cv2.COLOR_RGB2BGR))
+                    
+                    if simpleTest(encodedTemp,encodedTesters) == 0:
                         screenshotTemp.save('output/target{}.jpg'.format(fileNum))
+                        print("  [x2-x1]: ",x2-x1,"  [y2-y1]: ",y2-y1,"   Name:",face_names," FileNum:",fileNum," base:{",x1,y1,x2,y2,"}")
                     else:
-                        screenshotTemp.save('noise/noise{}.jpg'.format(fileNum))                
-                except:
-                    screenshotTemp.save('noise/noise{}.jpg'.format(fileNum))             
-                logLastImgNumber(int(fileNum)+1)
+                        screenshotTemp.save('noise/noise{}.jpg'.format(fileNum))             
+                    logLastImgNumber(int(fileNum)+1)
+                    
 
-        if keyboard.is_pressed('esc'):
-            cv2.destroyAllWindows()
-            break
-
-asyncio.run(execute_loop())
+        
+        
+        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+            print('You Pressed q Key!')
+            break  # finishing the loop
+    except:
+        break  # if user pressed a key other than the given key the loop will break
